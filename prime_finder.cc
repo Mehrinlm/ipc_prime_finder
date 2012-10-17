06cbfd9780595a292c54db4e448e9284c84a06d5
Luke Mehring <mehrinlm@uwec.edu>
2012-10-16 19:37:04 -0500

    Still no socket stuff but cleaned up code from internet a little and changed it so it will only do one iterations instead of keep iterating.  So it will do one at a time, then we can send it to other program

--- prime_finder.cc
+++ prime_finder.cc
@@ -5,9 +5,9 @@ using namespace std;
 
 //finds prime numbers using Sieve of Eratosthenes algorithm
 vector<int> calc_primes(const int min, const int max);
+void send_primes(vector<int> primes);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	cout << "Min number: ";
     int min;
 	cin >> min;
@@ -16,40 +16,53 @@ int main(int argc, char *argv[])
 	cin >> max;
 	
     vector<int> primes = calc_primes(min, max);
-
-    for(int i = 0; i < primes.size(); i++)
-    {
-        if(primes[i] != 0)
-                cout<<primes[i]<<endl;
+	
+	//Send across wire
+	send_primes(primes);
+	
+	
+	cout << "Sending:";
+	//This prints out primes to screen
+    for(int i = 0; i < primes.size(); i++) {
+        if(primes[i] != 0){
+			cout << " " << primes[i];
+		}	
     }
-
+	cout << endl;
     return 0;
 }
 
-vector<int> calc_primes(const int min, const int max)
-{
+/**
+ * Sends primes across wire to other program
+ * @param primes Vector of integers that represent primes (only send ones
+ * 					that are not zero)
+ */
+ void send_primes(vector<int> primes){
+	//do stuff
+ }
+
+/** 
+ *	Does one iteration and returns all the values remaining in 
+ *	the range that have not been ruled out.
+ */
+vector<int> calc_primes(const int min, const int max) {
     vector<int> primes;
 
     // fill vector with candidates
-    for(int i = min; i < max; i++)
-    {
+    for(int i = min; i < max; i++) {
         primes.push_back(i);
     }
 
-    // for each value in the vector...
-    for(int i = 0; i < primes.size(); i++)
-    {
-        //get the value
-        int v = primes[i];
+	//get the value
+	int v = primes[0];
 
-        if (v!=0) {
-                //remove all multiples of the value
-                int x = i+v;
-                while(x < primes.size()) {
-                        primes[x]=0;
-                        x = x+v;
-                }
-        }
-    }
+	if (v!=0) {
+			//remove all multiples of the value
+			int x = v;
+			while(x < primes.size()) {
+					primes[x]=0;
+					x = x+v;
+			}
+	}
     return primes;
 }
